3709f9b7ad18c03b6c0856c7e0deb6c32f22cbc4
SCWGxD <lockdown10007@gmail.com>
2025-11-10 20:32:11 +1000

    out

---
+++ src/net/cicada/module/impl/connect/Ping.java
@@ -0,0 +1,9 @@
+package net.cicada.module.impl.connect;
+
+import net.cicada.module.api.Category;
+import net.cicada.module.api.Module;
+import net.cicada.module.api.ModuleInfo;
+
+@ModuleInfo(name = "Ping", category = Category.Connect)
+public class Ping extends Module {
+}
