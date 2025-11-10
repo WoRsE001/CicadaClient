988073bc8dddb8690083e0b9b0518795b1c67f22
SCWGxD <lockdown10007@gmail.com>
2025-11-10 20:29:55 +1000

    add playerutil

---
+++ src/net/cicada/utility/PlayerUtil.java
@@ -0,0 +1,56 @@
+package net.cicada.utility;
+
+import lombok.experimental.UtilityClass;
+import net.cicada.module.api.ModuleManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.monster.EntityGhast;
+import net.minecraft.entity.monster.EntityGolem;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+
+@UtilityClass
+public class PlayerUtil implements Access {
+    public boolean isMob(Entity entity) {
+        return entity instanceof EntityMob
+                || entity instanceof EntityVillager
+                || entity instanceof EntitySlime
+                || entity instanceof EntityGhast
+                || entity instanceof EntityDragon;
+    }
+
+    public boolean isAnimal(Entity entity) {
+        return entity instanceof EntityAnimal
+                || entity instanceof EntitySquid
+                || entity instanceof EntityGolem
+                || entity instanceof EntityBat;
+    }
+
+    public boolean isTeam(Entity entity) {
+        if (mc.thePlayer.getDisplayName() != null && entity.getDisplayName() != null) {
+            String targetName = entity.getDisplayName().getFormattedText().replace("§r", "");
+            String clientName = mc.thePlayer.getDisplayName().getFormattedText().replace("§r", "");
+            return targetName.startsWith("§" + clientName.charAt(1));
+        }
+        return false;
+    }
+
+    public boolean isValid(Entity entity) {
+        if ((ModuleManager.TARGETS.teams.isValue() && isTeam(entity))) {
+            return false;
+        }
+        if (entity instanceof EntityLivingBase && entity.isEntityAlive() && entity != mc.thePlayer) {
+            if (ModuleManager.TARGETS.typeTargets.is("Players") && entity instanceof EntityPlayer) {
+                return true;
+            }
+            return (ModuleManager.TARGETS.typeTargets.is("Mobs") && isMob(entity)) || (ModuleManager.TARGETS.typeTargets.is("Animals")  && isAnimal(entity));
+        }
+        return false;
+    }
+}
