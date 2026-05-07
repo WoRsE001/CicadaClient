90ea7f00e8fb4b32f2466da453b582024f144042
SCWGxD <scwgxd@gmail.com>
2026-05-07 07:14:36 +0300

    с03 logger xd

--- src/main/kotlin/cicada/client/module/impl/player/ModuleExtraC03.kt
+++ src/main/kotlin/cicada/client/module/impl/player/ModuleExtraC03.kt
@@ -6,15 +6,26 @@ import cicada.client.module.Category
 import cicada.client.module.Module
 import cicada.client.utils.connection
 import cicada.client.utils.network.sendInvisiblePacket
+import cicada.client.utils.player
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket
 
 // SCWGxD regrets everything he did. 29.04.2026 11:17.
 object ModuleExtraC03 : Module("ExtraC03", Category.PLAYER) {
+    private val condition = multiChoice("Condition")
+    private val whileItemUse = condition.choice("While item use")
+    private val whileRegeneration = condition.choice("While regeneration")
+    private val whileBurning = condition.choice("While burning")
+    private val regenerationToHealth by int("Regeneration to health", 19, 0..20).visible { whileRegeneration.toggled }
+
     override fun onEvent(event: Event) {
         if (event is PacketEvent.Send) {
             val packet = event.packet
             if (packet is ServerboundMovePlayerPacket) {
-                connection.sendInvisiblePacket(packet)
+                if (
+                    (!whileItemUse.toggled || player.isUsingItem) &&
+                    (!whileRegeneration.toggled || player.health < regenerationToHealth) &&
+                    (!whileBurning.toggled || player.isOnFire)
+                ) connection.sendInvisiblePacket(packet)
             }
         }
     }
